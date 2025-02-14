30084a0f7d9c5fab0ed54b2f8896c50d3d5bb3f9
jay <junemassa@gmail.com>
2025-02-14 13:01:55 +0100

    applied styling

--- src/components/header.js
+++ src/components/header.js
@@ -4,19 +4,17 @@ import "../main.css";
 
 const Header = () => {
     return (
-        <header className="bg-white text-black p-3 fixed top-0 left-0 w-full z-10 flex items-center justify-between shadow-md">
+        <header className="bg-white text-black h-16 px-6 fixed top-0 left-0 w-full z-10 flex items-center justify-between shadow-md">
             {/* Left Side: Logo and Brand Name */}
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center space-x-4 flex-1"> 
                 <img src="/images/placeHolders/logoPlaceholder.jpg" alt="Logo" className="h-10 w-10" />
-                <img src="/images/placeHolders/logo_text.png" alt="Eduana" className="h-5" />
-            </div>
-
-            {/* Search Bar */}
-            <div className="flex-1 mx-4">
+                <img src="/images/placeHolders/logo_text.png" alt="Eduana" className="h-5 w-auto" />
+                
+                {/* Search Bar */}
                 <input
                     type="text"
                     placeholder="Search"
-                    className="border px-4 py-2 rounded-lg w-full max-w-md"
+                    className="border px-4 py-2 rounded-lg w-full max-w-md ml-6"
                 />
             </div>
 
