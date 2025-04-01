f986f4b8a8b873b7cdcdf69554b57fc967aadef0
PabloAtanes <144136968+PabloAtanes@users.noreply.github.com>
2025-04-01 12:41:30 +0200

    basics

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ import CourseOverview from "./pages/courseOverview";
 import CourseDetail from "./pages/courseDetail";
 import Notifications from "./pages/notifications.jsx";
 import NoPage from "./pages/noPage";
+import StudentManagement from "@/pages/studentMagementPage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -32,6 +33,7 @@ createRoot(document.getElementById("root")).render(
           <Route path="/courses" element={<CourseOverview />} />
           <Route path="/courses/:id/manage" element={<CourseDetail />} />
           <Route path="/notifications" element={<Notifications />} />
+          <Route path="/studentMagement" element={<StudentManagement/>}/>
           <Route path="*" element={<NoPage />} />
         </Routes>
       </main>
