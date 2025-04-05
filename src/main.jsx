87545c759ef0c29b7089de9790556da8eb39e943
j9mmy <127404933+j9mmy@users.noreply.github.com>
2025-04-05 16:38:32 +0200

    refactor: overhauled student management page

--- src/main.jsx
+++ src/main.jsx
@@ -15,17 +15,16 @@ import CourseOverview from "./pages/courseOverview";
 import CourseDetail from "./pages/courseDetail";
 import Notifications from "./pages/notifications.jsx";
 import NoPage from "./pages/noPage";
-import StudentManagement from "@/pages/studentMagementPage.jsx";
+import StudentManagement from "@/pages/student/manage/index.jsx";
 import ClassManagement from "@/pages/classePage.jsx";
 import LessonPreparation from "@/pages/lessonPrep.jsx";
 import StudentEvaluationTable from "@/pages/studentEvaluationPage.jsx";
 
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Navbar />
-      <main className="p-4">
+      <main className="flex flex-col min-h-screen">
+        <Navbar />
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/login" element={<TabbedLogin />} />
@@ -39,8 +38,8 @@ createRoot(document.getElementById("root")).render(
           <Route path="/notifications" element={<Notifications />} />
           <Route path="/students/manage" element={<StudentManagement />} />
           <Route path="/classes/manage" element={<ClassManagement />} />
-          <Route path="/lesson-preperation" element={<LessonPreparation/>} />
-          <Route path="/evaluation" element={<StudentEvaluationTable/>} />
+          <Route path="/lesson-preperation" element={<LessonPreparation />} />
+          <Route path="/evaluation" element={<StudentEvaluationTable />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
       </main>
---
+++ src/pages/student/manage/data.js
@@ -0,0 +1,7 @@
+export const studentsData = [
+  { id: 1, name: "Bonnie Green", age: 7, diet: null, experience: "High", language: "English" },
+  { id: 2, name: "John Smith", age: 8, diet: null, experience: "High", language: "Dutch" },
+  { id: 3, name: "Erik Kemp", age: 9, diet: null, experience: "Low", language: "Dutch" },
+  { id: 4, name: "Lana Byrd", age: 9, diet: "Halal", experience: "Very High", language: "Dutch" },
+  { id: 5, name: "Jese Leos", age: 9, diet: "Vegan", experience: "High", language: "Dutch" },
+];
